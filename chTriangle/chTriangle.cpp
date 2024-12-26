cbdb769ec3d0c3f5a579b873d3cd12ae2c0578a2
Hassaan Alvi <110996624+Randomiser47@users.noreply.github.com>
2024-12-26 07:20:22 +0500

    Add files via upload

---
+++ chTriangle/chTriangle.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    char ch ='A';
+
+    int n =5;
+
+    for (int i=0; i<n; i++)
+    {
+        for(int j =0; j < i+1; j++){
+        cout<<char(ch+i);
+    
+        }
+        cout<<endl;
+    }
+    
+
+     
+
+    return 0;
+}
